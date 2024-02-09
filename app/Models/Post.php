a2c3201bc2bbb0ca1b2892e4fe54a0c0b2012a9d
shouryo83 <mikisirex@gmail.com>
2024-02-09 23:56:18 +0900

    ブログ投稿の削除

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,9 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
+    use SoftDeletes;
+    
     public function getPaginateByLimit(int $limit_count = 10){
         return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);;
     }
@@ -16,5 +19,7 @@ class Post extends Model
         'body',
     ];
 }
+
+
 
 ?>
